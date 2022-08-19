6db5dbbf06515b55a2ca2dc28e2606204402b590
Carlos Alexandre de Souza <Carlos Alexandre de Souza>
2022-08-19 14:46:06 -0300

    first commit

--- src/Componentes/FooterComponent.js
+++ src/Componentes/FooterComponent.js
@@ -7,8 +7,48 @@ const FooterComponent = () => {
 
         <div class="footer">
 
+            <img src="./foto1.jpg" class="foto1"></img>
 
-            <h1>Footer</h1>
+            <h1 class="titulo">Obrigado pela sua visita.</h1>
+
+            <ul class="informacao1">
+
+
+                <li><a href="">Fotos</a></li>
+                <li>/</li>
+                <li><a href="">Vídeos</a></li>
+                <li>/</li>
+                <li><a href="">Notícias</a></li>
+                <li>/</li>
+                <li><a href="">Agenda para Shows</a></li>
+
+
+
+            </ul>
+
+
+            <h1 class="titulo3">Siga nosso trabalho nas nossas redes sociais.</h1>
+
+            <div class="redes">
+
+
+                <img src="./instagram.png" class="foto2"></img>
+                <img src="./facebook.png" class="foto3"></img>
+                <img src="./youtube.png" class="foto4"></img>
+                <img src="./twitter.png" class="foto5"></img>
+
+            </div>
+
+
+
+
+            <h1 class="titulo2">Contrate nosso show para seu evento.</h1>
+
+
+
+
+
+
 
 
 
--- src/Componentes/MainComponent.js
+++ src/Componentes/MainComponent.js
@@ -20,13 +20,49 @@ const MainComponent = () => {
 
                 <div class="caixa1">
 
-
+                    <h2>Ceilândia</h2>
+                    <p class="data1">16/04</p>
 
                 </div>
 
                 <div class="caixa2">
 
+                    <h2>Divinópolis</h2>
+                    <p class="data1">16/04</p>
 
+
+
+
+                </div>
+
+                <div class="caixa3">
+
+                    <h2>São Paulo</h2>
+                    <p class="data1">05/06</p>
+
+                </div>
+
+                <div class="caixa4">
+
+                    <h2>Rio de Janeiro</h2>
+                    <p class="data1">25/06</p>
+
+
+
+
+                </div>
+
+                <div class="caixa5">
+
+                    <h2>Porto Alegre</h2>
+                    <p class="data1">05/07</p>
+
+                </div>
+
+                <div class="caixa6">
+
+                    <h2>Goiânia</h2>
+                    <p class="data1">12/07</p>
 
 
 
